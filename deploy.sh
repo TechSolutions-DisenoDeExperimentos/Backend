9f529be413c1022a90f0e2fdd24816b78e3958d9
0renzo0loli0 <89040319+0renzo0loli0@users.noreply.github.com>
2024-06-27 17:04:50 -0500

    fix: improve deploy commands

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -jar ./target/TuCine-0.0.1-SNAPSHOT.jar &>/tmp/output.log &
+nohup java -jar ./target/TuCine-0.0.1-SNAPSHOT.jar &>/tmp/output.log &
 FOO_PID=$!
 echo "PID: $FOO_PID"
 echo $(jobs)
